bc2ca3ba0c8c4f95484585db97b3c3acf76b53d7
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2025-11-10 02:40:43 -0600

    count words basic

--- word-count/src/main.rs
+++ word-count/src/main.rs
@@ -1,16 +1,43 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    if let Ok(lines) = read_lines("./example.txt") {
-        for line in lines.map_while(Result::ok) {
-            println!("{}", line)
-        }
-    }
+    process_file("./book.txt");
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P:AsRef<Path>, {
+fn process_file(filename: &str) {
+    let mut my_map = HashMap::<String, u32>::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            process_line(&mut my_map, line);
+        }
+    }
+
+    println!("my map: {:?}", my_map);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn process_line(my_map: &mut HashMap<String, u32>, line: String) {
+    if line == "" {
+        return;
+    }
+
+    for part in line.split(" ") {
+        let tmp = part.to_lowercase();
+        if let Some(x) = my_map.get_mut(&tmp) {
+            *x = *x + 1;
+        } else {
+            my_map.insert(String::from(&tmp), 1);
+        }
+    }
 }
