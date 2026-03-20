ed41947e92597647ae17fc0eb18fab6e1a672c73
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-03-20 03:07:22 -0600

    fmt

--- identicon/src/main.rs
+++ identicon/src/main.rs
@@ -4,10 +4,9 @@ fn main() {
     match identicon::run("shushu") {
         Ok(_) => {
             println!("sucess");
-        },
+        }
         Err(e) => {
             panic!("error: {}", e)
         }
-        
     }
 }
