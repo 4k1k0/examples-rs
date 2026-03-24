533524c39a66bf5306ea45c25e83fbd4354e3506
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-03-24 04:49:30 -0600

    docker

--- identicon/src/main.rs
+++ identicon/src/main.rs
@@ -1,9 +1,45 @@
+use tonic::{Request, Response, Status, transport::Server};
+
+use identiconpro::identiconer_server::{Identiconer, IdenticonerServer};
+use identiconpro::{IdenticonReply, IdenticonRequest};
+
+pub mod identiconpro {
+    tonic::include_proto!("server");
+}
+
 mod identicon;
 
-fn main() {
-    let username = std::env::args().nth(1).expect("no username given");
-    match identicon::run(&username) {
-        Ok(_) => println!("success"),
-        Err(e) => panic!("error: {}", e),
+#[derive(Debug, Default)]
+pub struct MyIdenticoner {}
+
+#[tonic::async_trait]
+impl Identiconer for MyIdenticoner {
+    async fn create(
+        &self,
+        request: Request<IdenticonRequest>,
+    ) -> Result<Response<IdenticonReply>, Status> {
+        println!("request: {:?}", request);
+
+        let path = match identicon::run(&request.get_ref().username) {
+            Ok(_) => "good".to_string(),
+            Err(e) => e.to_string(),
+        };
+
+        let res = IdenticonReply { path };
+
+        Ok(Response::new(res))
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let identi = MyIdenticoner::default();
+
+    Server::builder()
+        .add_service(IdenticonerServer::new(identi))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
