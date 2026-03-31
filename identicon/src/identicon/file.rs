2cd5f373005d4bca38cebf9182489783545f407f
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-03-31 03:24:53 -0600

    save in img dir

--- identicon/src/identicon/file.rs
+++ identicon/src/identicon/file.rs
@@ -3,7 +3,7 @@ use string_builder::Builder;
 pub fn generate_filename(hash: [u8; 16]) -> String {
     let mut builder = Builder::default();
 
-    builder.append("/tmp/");
+    builder.append("images/");
 
     for h in hash {
         let tmp = format!("{}", h);
